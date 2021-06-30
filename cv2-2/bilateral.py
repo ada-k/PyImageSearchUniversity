8e07e51ab1881cbc865446ccb33cfc14542fdd38
ada-k <adakibetj@protonmail.com>
2021-06-30 10:12:59 +0300

    smoothing and blurring

--- cv2-2/bilateral.py
+++ cv2-2/bilateral.py
@@ -10,8 +10,19 @@ arg.add_argument("-o", "--path", default = "save.png")
 args = vars(arg.parse_args())
 
 image = cv2.imread(args['image'])
-# convert image to grayscale -- single channel image.
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow("original", image)
-cv2.imshow("single channel", gray)
 cv2.waitKey(0)
+
+## reduces noise but maintains edges. -- introduces 2 gaussian dists: 
+### spatial neigbours and pixel intensity
+#### used in image cartoonisation
+params = [(11, 21, 7), (11, 41, 21), (11, 61, 39)]
+
+for (diameter, sigmaColor, sigmaSpace) in params:
+    blurred = cv2.bilateralFilter(image, diameter, sigmaColor, sigmaSpace)
+    cv2.imshow(f"{(diameter, sigmaColor, sigmaSpace)} - bilateral blurred", blurred)
+    cv2.waitKey(0)
+
+cv2.destroyAllWindows()
+cv2.imshow("original", image)
+
--- cv2-2/blurring.py
+++ cv2-2/blurring.py
@@ -10,8 +10,38 @@ arg.add_argument("-o", "--path", default = "save.png")
 args = vars(arg.parse_args())
 
 image = cv2.imread(args['image'])
-# convert image to grayscale -- single channel image.
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow("original", image)
-cv2.imshow("single channel", gray)
 cv2.waitKey(0)
+
+image = cv2.imread(args['image'])
+cv2.imshow("original", image)
+cv2.waitKey(0)
+
+kernel_sizes = [(3,3), (9,9), (15,15)] # the larger the kernel, the blurrier the image - can be rect as long as the values are odd
+
+# basic avg blur
+for (kx, ky) in kernel_sizes:
+    blurred = cv2.blur(image, (kx, ky))
+    cv2.imshow(f"{(kx, ky)} - blurred", blurred)
+    cv2.waitKey(0)
+
+cv2.destroyAllWindows()
+cv2.imshow("original", image)
+
+# Gaussian Blur  -  #gives more weight to the pixels at the centre of the kernel
+for (kx, ky) in kernel_sizes:
+    gaussian_blurred = cv2.GaussianBlur(image, (kx, ky), 0) # 0 tells cv2 to automatically compute sigma using our kernel size
+    cv2.imshow(f"{(kx, ky)} - gaussian_blurred", gaussian_blurred)
+    cv2.waitKey(0)
+
+cv2.destroyAllWindows()
+cv2.imshow("original", image)
+
+# Median Blur  - 
+for k in (3, 9, 15):
+    median_blurred = cv2.medianBlur(image, k) # 0 tells cv2 to automatically compute sigma using our kernel size
+    cv2.imshow(f"{(kx, ky)} - median_blurred", median_blurred)
+    cv2.waitKey(0)
+
+cv2.destroyAllWindows()
+cv2.imshow("original", image)
